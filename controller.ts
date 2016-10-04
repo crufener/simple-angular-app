446b08273c78d6b843c192b42ec55c20346b9feb
Craig Rufener <crufener@outlook.com>
2016-10-04 10:59:19 -0700

    Initial setup
    Setting up with bower, typings, angular, bootstrap

---
+++ controller.ts
@@ -0,0 +1,15 @@
+namespace myApp {
+
+    class MovieListController {
+        public movies;
+
+        constructor() {
+            this.movies = [
+                {id: 1, title: 'Batman', director: 'Nolan' },
+                {id: 2, title: 'Superman', director: 'Scott' },
+                {id: 3, title: 'Spiderman', director: 'Wells' }
+            ];
+        }
+    }
+    angular("MovieApp").controller("MovieListController", MovieListController);
+}
